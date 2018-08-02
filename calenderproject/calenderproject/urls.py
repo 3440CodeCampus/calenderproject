d9030a8ed8270fe1f00bc90b56716b30f385b4d8
3440CodeCampus <mtlgdroid@gmail.com>
2018-08-01 22:23:30 -0400

    link between list and detail

--- calenderproject/calenderproject/urls.py
+++ calenderproject/calenderproject/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="index"),
+    path('entry/<int:pk>', views.detail, name="detail"),
     path('admin/', admin.site.urls),
 ]
--- calenderproject/myapp/views.py
+++ calenderproject/myapp/views.py
@@ -5,3 +5,8 @@ from .models import Entry
 def index(request):
     events = Entry.objects.all()
     return render(request, 'myapp/index.html', {'events':events})
+
+def detail(request,pk):
+    detail = Entry.objects.get(pk=pk)
+    
+    return render(request, 'myapp/detail.html',{'detail':detail})

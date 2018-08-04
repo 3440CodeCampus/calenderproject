0096d59b4a8912e9782880ff9804751ece1efc67
3440CodeCampus <mtlgdroid@gmail.com>
2018-08-03 21:34:13 -0400

    move detail button from index to _calender_entry

--- calenderproject/calenderproject/urls.py
+++ calenderproject/calenderproject/urls.py
@@ -20,6 +20,7 @@ from myapp import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('entry/add',views.add, name="add"),
+    path('entry/delete/<int:pk>', views.delete, name='delete'),
     path('entry/<int:pk>', views.detail, name="detail"),
     path('admin/', admin.site.urls),
 ]

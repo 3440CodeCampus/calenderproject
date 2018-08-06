8fed9467fe293ad6f7504d37cf2fe6656c57e286
3440CodeCampus <mtlgdroid@gmail.com>
2018-08-06 11:02:32 -0400

    datepicker is available

--- calenderproject/myapp/forms.py
+++ calenderproject/myapp/forms.py
@@ -1,6 +1,15 @@
 from django import forms
+from django.contrib.admin.widgets import AdminDateWidget
 
 class EntryForm(forms.Form):
-    name = forms.CharField(max_length=50)
-    date = forms.DateTimeField()
+    name = forms.CharField(max_length=50,
+        widget=forms.TextInput(
+            attrs={
+                'style':'border-color:blue',
+                'placeholder':'這裏填你 叫乜水?'
+            }
+        )
+    )
+    date = forms.DateField(widget=AdminDateWidget())
+    
     description = forms.CharField(widget=forms.Textarea)
